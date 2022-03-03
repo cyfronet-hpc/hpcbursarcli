237fc8b224073ba08ccc327dc602b231d81cfd44
szymon.budziak@cyfronet.pl <szymon.budziak@cyfronet.pl>
2022-03-03 11:41:45 +0100

    Added hpcbursar.sh file

---
+++ hpcbursar.sh
@@ -0,0 +1,3 @@
+export HPC_BURSAR_LIBDIR=/opt/cyfronet/hpcbursar.venv/lib/python3.9/site-packages
+export HPC_BURSAR_URL=https://slurm01.ares.cyfronet.pl/hpcbursar/api/v1/
+export HPC_BURSAR_CERT_PATH=/etc/grid-security/certificates/4ba9a797.0
